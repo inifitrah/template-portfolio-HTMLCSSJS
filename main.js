20d4111f775498b5c504bdcf69464d934c72809a
Muh. Fitrah Ramadhan <mftrhr@gmail.com>
2023-06-16 09:25:59 +0800

    done untuk tampilannya maupun respoonsivenya cuma fontnya aneh

---
+++ main.js
@@ -0,0 +1,35 @@
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x')
+  navbar.classList.toggle('active')
+}
+
+let sections = document.querySelectorAll('section')
+let navLink = document.querySelectorAll('header nav a')
+
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 150
+    let height = sec.offsetHeight
+    let id = sec.getAttribute('id')
+
+    if(top >= offset && top < offset + height){
+      navLink.forEach(links => {
+        links.classList.remove('active')
+        document.querySelector('header nav a[href*=' + id +']').classList.add('active')
+       })
+    }
+  })
+
+  // sticky navbar
+  let header = document.querySelector('header')
+  header.classList.toggle('sticky', window.scrollY > 100)
+
+  // remove toggle icon navbar whem click nsvbsr link
+  menuIcon.classList.remove('bx-x')
+  navbar.classList.remove('active')
+}
