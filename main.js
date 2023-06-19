daa08dfe882f806ef428ea67af0a3f4bc53dd9bb
Muh. Fitrah Ramadhan <mftrhr@gmail.com>
2023-06-19 17:10:02 +0800

    bug hover wkwwk

--- main.js
+++ main.js
@@ -1,23 +1,116 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
-let portLayer = document.querySelector("#port-layer");
-let arrow = document.querySelector("#arrow");
+
 let modes = document.getElementById("modes");
+
 
 modes.addEventListener("click", function () {
   modes.classList.toggle("bxs-sun");
   document.body.classList.toggle("active-mode");
 });
 
-arrow.onclick = () => {
-  arrow.classList.toggle("bx-x");
 
-  if (arrow.classList.contains("bx-x")) {
-    portLayer.style.transform = "translateY(0%)";
-  } else {
-    portLayer.style.transform = "translateY(100%)";
+let portLayer = document.querySelectorAll(".portfolio-layer");
+let arrows = document.querySelectorAll(".arrows");
+console.log(arrows);
+
+arrows.forEach((arr, index) => {
+  console.log(arr)
+  console.log(index)
+  arr.onclick = () => {
+    arr.classList.toggle("bx-x");
+    portLayer[index].classList.toggle("translated");
+  };
+});
+
+document.addEventListener("click", (event) => {
+  if (!event.target.closest(".arrows")) {
+    portLayer.forEach((element) => {
+      element.classList.remove("translated");
+    });
+    arrows.forEach((arr) => {
+      arr.classList.remove("bx-x");
+    });
   }
-};
+});
+
+document.addEventListener("scroll", () => {
+  portLayer.forEach((element) => {
+    element.classList.remove("translated");
+  });
+  arrows.forEach((arr) => {
+    arr.classList.remove("bx-x");
+  });
+});
+
+
+
+
+
+// let portLayer = document.querySelectorAll(".portfolio-layer");
+// let arrows = document.querySelectorAll(".arrows");
+// console.log(arrows);
+
+// arrows.forEach((arr) => {
+//   console.log("console arr" + arr)
+//   arr.onclick = () => {
+//     arr.classList.toggle("bx-x");
+
+//     portLayer.forEach((element) => {
+//       console.log(element);
+//       if (arr.classList.contains("bx-x")) {
+//         element.classList.add("translated");
+//       } else {
+//         element.classList.remove("translated");
+//       }
+//     });
+//   };
+// });
+
+
+
+
+// let portLayer = document.querySelectorAll(".portfolio-layer");
+// let arrows = document.querySelectorAll(".arrows");
+// console.log(arrows)
+// arrows.onclick = () => {
+//   arrows.forEach((arr) => {
+
+
+//     arr.classList.toggle("bx-x");
+  
+//     portLayer.forEach((element) => {
+//       console.log(element)
+//       if (arrows.classList.contains("bx-x")) {
+//         element.classList.add("translated");
+//       } else {
+//         element.classList.remove("translated");
+//       }
+//     });
+//   }
+    
+// )}
+
+
+
+// let portLayer = document.querySelectorAll(".portfolio-layer");
+// let arrow = document.querySelector("#arrow");
+
+// arrow.onclick = () => {
+//   arrow.classList.toggle("bx-x");
+
+//   portLayer.forEach((anu) => { 
+  
+    
+//     for (i = 0; i < anu.length; i++) {
+//       if (arrow.classList.contains("bx-x")) {
+//         anu.style.transform = "translateY(0%)";
+//       } else {
+//         anu.style.transform = "translateY(100%)";
+//       }
+//     }
+//   }) 
+// };
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
