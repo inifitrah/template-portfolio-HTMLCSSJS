df9dd163790e865df892a1c9d3cdc9429cd81a3d
Muh. Fitrah Ramadhan <mftrhr@gmail.com>
2023-07-09 09:07:10 +0800

    fix position icon modes agar tidak absolute

--- main.js
+++ main.js
@@ -4,51 +4,41 @@ const imgAbout = document.querySelector(".about img");
 const portLayer = document.querySelectorAll(".portfolio-layer");
 const arrows = document.querySelectorAll(".arrows");
 const image = document.getElementById("myLogoImage");
-
-// mode
 const modes = document.getElementById("modes");
-// next kalau ini
-const prefersDarkMode =
+const systemDarkMode =
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme: dark)").matches;
-// console.log(prefersDarkMode)
-  
-
-// modal
 const containerModal = document.querySelector(".container-modal");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".btn-modal");
-
-const openModal = () => {
+const openModal = (callback) => {
   containerModal.classList.remove("hidden");
+  callback()
 };
 const closeModal = () => {
-  containerModal.classList.add("hidden")
+  containerModal.classList.add("hidden");
 };
-
+// onload = openModal;
 onload = setTimeout(() => {
-  openModal();
-}, 5000);
-
+   openModal()
+}, 4000)
 // modal
 document.addEventListener("click", (e) => {
-  //  if(e.target == modes) openModal()
-  if (e.target == btnClose) closeModal();
-  if (e.target == overlay) closeModal();
+  if (e.target.classList.contains("btn-modal")) closeModal();
+  if (e.target.classList.contains("overlay")) closeModal();
 });
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape" && !containerModal.classList.contains("hidden"))
     closeModal();
 });
+// modal akhir
 
-// versi document
+// mode page
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("bxs-moon")) {
     e.target.classList.toggle("bxs-sun");
-    document.body.classList.toggle("active-mode")
-  
-
+    document.body.classList.toggle("active-mode");
     function toggleImage() {
       if (image.src.includes("img/logo/trahdark.png")) {
         image.src = "img/logo/trah.png";
@@ -58,13 +48,15 @@ document.addEventListener("click", (e) => {
     }
     toggleImage();
   }
-
+  // last
+  // nav menu
   if (e.target.classList.contains("bx-menu")) {
     e.target.classList.toggle("bx-x");
     navbar.classList.toggle("active");
   }
 });
 
+// arrow di portfbox
 arrows.forEach((arr, index) => {
   arr.addEventListener("mouseenter", () => {
     arr.classList.add("bx-x");
@@ -76,10 +68,11 @@ arrows.forEach((arr, index) => {
     portLayer[index].classList.toggle("translated");
   });
 });
+// last
 
+//
 const sections = document.querySelectorAll("section");
 const navLink = document.querySelectorAll("header nav a");
-
 window.onscroll = () => {
   sections.forEach((sec) => {
     const top = window.scrollY;
@@ -97,6 +90,7 @@ window.onscroll = () => {
     }
   });
 
+  // delete th transform
   portLayer.forEach((element) => {
     element.classList.remove("translated");
   });
@@ -113,25 +107,24 @@ window.onscroll = () => {
   navbar.classList.remove("active");
 };
 
-// // libary
+// // library
 // // typing
-  const typing = new Typed(".typingjs", {
-    strings: ["Web Developer", "FullStack JS"],
-    typeSpeed: 100,
-    cursorChar: "|",
-  });
-  //  scroll reveal
+const typing = new Typed(".typingjs", {
+  strings: ["Web Developer", "FullStack JS"],
+  typeSpeed: 100,
+  cursorChar: "|",
+});
 
-  ScrollReveal({
-    distance: "80px",
-    duration: 2000,
-    delay: 200,
-  });
+//  scroll reveal
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
 
-  ScrollReveal().reveal(".home-content", { origin: "top" });
-  ScrollReveal().reveal(".home-content h1", { origin: "left" });
-  ScrollReveal().reveal(".home-img", { origin: "bottom" });
-  ScrollReveal().reveal(".about-content h3", { origin: "left" });
-  ScrollReveal().reveal(".about .about-img", { origin: "bottom" });
-  ScrollReveal().reveal(".skills-box, .portfolio-box", { origin: "bottom" });
-  
+ScrollReveal().reveal(".home-content", { origin: "top" });
+ScrollReveal().reveal(".home-content h1", { origin: "left" });
+ScrollReveal().reveal(".home-img", { origin: "bottom" });
+ScrollReveal().reveal(".about-content h3", { origin: "left" });
+ScrollReveal().reveal(".about .about-img", { origin: "bottom" });
+ScrollReveal().reveal(".skills-box, .portfolio-box", { origin: "bottom" });
